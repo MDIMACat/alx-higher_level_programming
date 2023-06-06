2c5ac337d494ff5c04dbc5c15d63e4ec1493d8e3
MDIMACat <romanmdima@gmail.com>
2023-06-06 14:42:31 +0200

    Corrected Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_number = abs(number) % 10
-    return last_number
+    print("{}".format(last_number), end="")
+    return(abs(number) % 10)
