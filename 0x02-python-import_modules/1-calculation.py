eb63f79ae391220e5570a5effc0d9d7cfb384f27
MDIMACat <romanmdima@gmail.com>
2023-06-08 12:17:31 +0200

    comment updates

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from calculator_1 import add, sub, mul, div
-	a = 10
-	b = 5
-	total = add(a, b)
-	print("{:d} + {:d} = {:d}".format(a, b, total))
-	total = sub(a, b)
-	print("{:d} - {:d} = {:d}".format(a, b, total))
-	total = mul(a, b)
-	print("{:d} * {:d} = {:d}".format(a, b, total))
-	total = div(a, b)
-	print("{:d} / {:d} = {:d}".format(a, b, total))
+    """print the total sum, subtraction , mulitplications
+    and  division of a and b"""
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    total = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, total))
+    total = sub(a, b)
+    print("{:d} - {:d} = {:d}".format(a, b, total))
+    total = mul(a, b)
+    print("{:d} * {:d} = {:d}".format(a, b, total))
+    total = div(a, b)
+    print("{:d} / {:d} = {:d}".format(a, b, total))
