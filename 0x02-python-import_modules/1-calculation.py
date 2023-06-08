a81c8d2d30057d3b4bdafacd8d4c41954e002fe7
MDIMACat <romanmdima@gmail.com>
2023-06-08 12:09:21 +0200

    Udpdated Task 0 - 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-
-total = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, total))
-total = sub(a, b)
-print("{:d} - {:d} = {:d}".format(a, b, total))
-total = mul(a, b)
-print("{:d} * {:d} = {:d}".format(a, b, total))
-total = div(a, b)
-print("{:d} / {:d} = {:d}".format(a, b, total))
+if __name__ == "__main__":
+	from calculator_1 import add, sub, mul, div
+	a = 10
+	b = 5
+	total = add(a, b)
+	print("{:d} + {:d} = {:d}".format(a, b, total))
+	total = sub(a, b)
+	print("{:d} - {:d} = {:d}".format(a, b, total))
+	total = mul(a, b)
+	print("{:d} * {:d} = {:d}".format(a, b, total))
+	total = div(a, b)
+	print("{:d} / {:d} = {:d}".format(a, b, total))
