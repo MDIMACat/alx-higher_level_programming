f601ee3a51a1eb98c2f4d52e5d5bf3830960df45
MDIMACat <romanmdima@gmail.com>
2023-06-14 14:15:51 +0200

    Task 7 updated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary["key"] = value
-    return a_dictionary
+    for key in range(len(a_dictionary)):
+        if key in a_dictionary:
+            a_dictionary["key"] = value
+        else:
+            a_dictionary["key"] = value
+        return a_dictionary
+
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
