1c98325fd38bfe773330aa5441a2f519cf10878e
MDIMACat <romanmdima@gmail.com>
2023-06-14 14:22:38 +0200

    7 updated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for key in range(len(a_dictionary)):
-        if key in a_dictionary:
-            a_dictionary["key"] = value
-        else:
-            a_dictionary["key"] = value
-        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
