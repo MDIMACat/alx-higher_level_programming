df40220a16663bf84ab79a5e3373af71275012d5
MDIMACat <romanmdima@gmail.com>
2023-07-03 12:40:30 +0200

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" Defines a rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle
+
+    Args: Width (int) size of width
+        height (int) size of height
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Get the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Gets the area of a rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Gets the perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns the printable representation of string"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle_str = ""
+        for _ in range(self.height):
+            rectangle_str += "#" * self.width + "\n"
+        return rectangle_str
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
