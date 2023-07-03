bc0526493b5d01c2e1fb526867e6f4fa4c2ead95
MDIMACat <romanmdima@gmail.com>
2023-07-03 12:29:55 +0200

    Task 1 revision

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
         Args: Width (int) size of rectangle
             height (int) size of a rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
