65247ffba65ca292bd704c70bb13eaa68898a211
MDIMACat <romanmdima@gmail.com>
2023-07-07 13:57:08 +0200

    task 3...TDD

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a function say_my_name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function returns a persons first and last name
+    
+    Args:
+        first_name (str): this is the variable for first_name
+        last_name(str): variable for last_name
+    
+    Return: First and last name
+    
+    Raises a TypeError if input isnt a string"""
+    
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    print("My name is {} {}".format(first_name, last_name))
+    
+    
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -4,6 +4,5 @@
 
     0. Integers addition 
         Write a function that adds 2 integers.
-    1. Divide a matrix 
-        Write a function that divides all elements of a matrix.
-        
+    2. Say my name 
+        Write a function that prints My name is <first name> <last name>
