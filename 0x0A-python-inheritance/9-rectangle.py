3f876440c1cec6b8338864b5946088a81ffb9038
MDIMACat <romanmdima@gmail.com>
2023-09-11 14:03:29 +0200

    Added documentation and modified str representation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,13 +10,29 @@ class Rectangle(BaseGeometry):
     Implementation for Rectangle
     """
     def __init__(self, width, height):
+        """
+        Instantiates a Rectangle with a given width and height.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """
+        Computes the area of the rectangle.
+        Returns:
+            int: The area of the rectangle.
+        """
         return self.__width * self.__height
 
     def __str__(self):
-        return "[{}] {}/{}".format(self.__name__, self.__width, self.__height)
+        """
+        Returns a string representation of the rectangle.
+        Returns:
+            str: A string in the format "[Rectangle] <width>/<height>"
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
