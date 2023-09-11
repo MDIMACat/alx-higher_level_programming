00fcb7e8489297cac8cc24bcbac7e2d493bb7f9f
MDIMACat <romanmdima@gmail.com>
2023-09-11 13:05:43 +0200

    Continued from base Geo file, added area module

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Implementing a base_geometry class"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
