df534a20e678a229326ae253b884fae73d749dd1
MDIMACat <romanmdima@gmail.com>
2023-09-12 14:52:40 +0200

    Updated task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module The gives __dict__ of simple data structure"""
+"""Module that gives __dict__ of simple data structure"""
 
 
 def class_to_json(obj):
     """Implementation of the class_to_json module"""
-    new_dict = obj.__dict__()
+    new_dict = obj.__dict__
     return new_dict
