8396bca8692e427b5afc479ce4511ce57dd6fb2d
MDIMACat <romanmdima@gmail.com>
2023-09-12 13:26:35 +0200

    Added documentation to task 5, save_to_json_file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Implemented module save_to_json_file"""
     with open(filename, "w", encoding="utf-8") as file1:
         content = json.dumps(my_obj)
     return content
