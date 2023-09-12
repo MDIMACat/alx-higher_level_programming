02df2a5deb3139553b85746fdc8d597207990706
MDIMACat <romanmdima@gmail.com>
2023-09-12 09:58:24 +0200

    Implemented a read file module in 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module to read file"""
+
+
+def read_file(filename=""):
+    """Implementing module that reads file"""
+    if filename is not None:
+        with open(filename, "r", encoding="utf-8") as file1:
+            content = file1.read()
+            print(content)
