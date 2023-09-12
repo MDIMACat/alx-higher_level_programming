67e4a730cbe469bc0ce77f096e80b96742cff87a
MDIMACat <romanmdima@gmail.com>
2023-09-12 13:28:29 +0200

    Added the write() to the file1 in task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """Implemented module save_to_json_file"""
     with open(filename, "w", encoding="utf-8") as file1:
         content = json.dumps(my_obj)
-    return content
+        file1.write(content)
