0193e0d670110ad5baaaf1f65c50dd3aecdd3a24
MDIMACat <romanmdima@gmail.com>
2023-10-18 13:44:22 +0200

    task 16 implemented

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,16 +1,9 @@
 --  a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-SELECT
-  tv_shows.title,
-  IFNULL(tv_genres.name, 'NULL') AS genre_name
-FROM
-  tv_shows
-LEFT JOIN
-  tv_show_genres
-ON
-  tv_shows.id = tv_show_genres.show_id
-LEFT JOIN
-  tv_genres
-ON
-  tv_show_genres.genre_id = tv_genres.id
-ORDER BY
-  tv_shows.title ASC, genre_name ASC;
+SELECT	title,
+	name
+FROM	tv_shows
+	LEFT OUTER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+		LEFT OUTER JOIN tv_genres
+		ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title, name;
