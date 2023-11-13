0b62ead7f4cebbe6280babb6e3463e46d62189fb
MDIMACat <romanmdima@gmail.com>
2023-11-13 17:39:36 +0200

    changed the name from count to argc

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const count = process.argv.lenth;
+const argc = process.argv.length;
 
-if (count === 2) {
-    console.log('No argument');
-} else if (count === 3) {
-    console.log('Argument found');
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
