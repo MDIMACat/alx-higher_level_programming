9d376944aff5abb3a6da94c9fec44c75907b0586
MDIMACat <romanmdima@gmail.com>
2023-11-13 19:02:00 +0200

    Task 10 try

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial(number) {
+  if (number === 1 || isNaN(number)) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+const inputNumber = parseInt(process.argv[2]);
+
+console.log(factorial(inputNumber));
+
