45685ef912948e8401130652e0012c3d6314f131
MDIMACat <romanmdima@gmail.com>
2023-11-13 17:38:06 +0200

    corrected errors in task 2 of javascript project

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+const count = process.argv.lenth;
 
-const count = process.argv.lenth - 2;
-
-if (count === 0) {
-    console.log('No argument')
-} else if (count === 1) {
-    console.log('Argument found') 
+if (count === 2) {
+    console.log('No argument');
+} else if (count === 3) {
+    console.log('Argument found');
 } else {
-    console.log('Arguments found')
+    console.log('Arguments found');
 }
