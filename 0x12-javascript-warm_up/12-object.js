92c4427a84a675d5b9816622d098354fbba2f24c
MDIMACat <romanmdima@gmail.com>
2023-11-13 19:14:06 +0200

    finished project

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const theObject = {
+  type: 'object',
+  value: 12
+};
+console.log(theObject);
+theObject.value = 89;
+console.log(theObject);
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+exports.add = function (One, Two) {
+  return (One + Two);
+};
