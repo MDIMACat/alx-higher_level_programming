5222cf291197dbc50456516c30e5a8b7b99b740a
MDIMACat <romanmdima@gmail.com>
2023-11-13 18:48:39 +0200

    Implemented task 8 and 9

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+let counter = 0;
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (counter = 0; counter < size; counter++) {
+    console.log('X'.repeat(size));
+  }
+}
+
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const number1 = process.argv[2];
+const number2 = process.argv[2];
+
+console.log(parseInt(number1) + parseInt(number2));
