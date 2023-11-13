52ebe5cca354ee743b1b749e9a288b7e40e87a94
MDIMACat <romanmdima@gmail.com>
2023-11-13 18:38:24 +0200

    Changed task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
 
-for (i = 0; i < list.length; i++) {
-    console.log(list[i]);
+for (let i = 0; i < list.length; i++) {
+  console.log(list[i]);
 }
