53e67cb75d71b79016532a6d9f1ffd093d7533fe
MDIMACat <romanmdima@gmail.com>
2023-11-13 17:46:09 +0200

    Printing from the command line - updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined) {
+const arg = process.argv[2]
+if (arg === undefined) {
   console.log("No argument");
 } else {
-  console.log(process.argv[2]);
+  console.log(arg);
 }
 
