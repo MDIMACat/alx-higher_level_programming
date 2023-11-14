71e937085a24d95fcff642f394ea302ce676f26f
MDIMACat <romanmdima@gmail.com>
2023-11-14 12:16:20 +0200

    Task 2 - created a class with a constructor for width and height, added a if conditional in regards to the attributes of the class

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w === undefined || w < 1 || h === undefined || h < 1) {
+      return;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
