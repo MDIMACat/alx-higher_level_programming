6b3d5c4b458626d415851937de864f408ecce5c8
MDIMACat <romanmdima@gmail.com>
2023-11-14 12:54:01 +0200

    Task 3 - continuation of task 2, added a print method to the class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w === undefined || w < 1 || h === undefined || h < 1) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+  print() {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
