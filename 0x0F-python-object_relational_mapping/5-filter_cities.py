b0e6e87354613b38e2e350b5cf3bc70e01e5cb22
MDIMACat <romanmdima@gmail.com>
2023-11-17 09:59:44 +0200

    Task 5 - script that takes in the name of a state as an argument and lists all cities of that state, using the database

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+script that takes in the name of a state as an argument
+and lists all cities of that state, using the database
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if (__name__ == "__main__"):
+
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwrd=password,
+        db=db_name,
+    )
+
+    cursor = connection.cursor()
+    query = "SELECT cities.name\
+                            FROM states INNER JOIN cities\
+                            ON states.id = state_id\
+                            WHERE states.name = %s;"
+    cursor(query, state_name)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
