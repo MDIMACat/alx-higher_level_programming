8ae7a51e3c44e8ee4503b1cb4401e1bf3b763cd7
MDIMACat <romanmdima@gmail.com>
2023-11-17 14:14:41 +0200

    Deletes all objects containing an A in it

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Deletes all with the 'a' in database
+"""
+
+
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.contains('a'))
+    for state in states:
+        session.delete(state)
+
+    session.commit()
