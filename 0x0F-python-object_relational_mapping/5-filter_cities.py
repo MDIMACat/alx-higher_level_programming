122709774d770f32c66c8c66b39d4751d008f97b
MDIMACat <romanmdima@gmail.com>
2023-11-17 10:29:18 +0200

    5 - Revised

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
 """
-Lists all cities for the given state.
-The MySQL username, password, database name
-and the searched state are given as arguments
-to the module.
+Script that takes in the name of a state as an argument
+and lists all cities of that state, using the database
 """
 
-
+import MySQLdb
 from sys import argv
-from MySQLdb import connect
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     cities = ""
 
-    with connect(db=argv[3]) as connection:
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    with MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+    ) as connection:
         with connection.cursor() as cursor:
+            query = """
+                    SELECT cities.name
+                    FROM cities
+                    JOIN states ON cities.state_id = states.id
+                    WHERE states.name = %s
+                    ORDER BY cities.id;
+                    """
 
-            cursor.execute("SELECT cities.name\
-                            FROM states INNER JOIN cities\
-                            ON states.id = state_id\
-                            WHERE states.name = %s;",
-                           (argv[4],))
+            cursor.execute(query, (state_name,))
 
-            for row in cursor.fetchall():
-                cities += f"{row[0]}, "
+            cities = cursor.fetchall()
+            cursor.close()
+            connection.close()
 
-            print(cities[0:-2])
+        if cities:
+            print(", ".join(city[0] for city in cities))
