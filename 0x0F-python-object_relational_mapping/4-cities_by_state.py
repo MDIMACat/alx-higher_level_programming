a15a9014abc84318846d4c00c0547821a93ef496
MDIMACat <romanmdima@gmail.com>
2023-11-17 08:26:14 +0200

    Revised task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-""" script that lists all cities from the database
-ARGS: mysql username, mysql password and database name
 """
-
+Script that lists all cities from the database.
+ARGS: mysql username, mysql password, and database name
+"""
 
 import MySQLdb
 from sys import argv
 
-if (__name__ == "__main__"):
-
+if __name__ == "__main__":
     username = argv[1]
     password = argv[2]
     db_name = argv[3]
 
     connection = MySQLdb.connect(
         host="localhost",
-        port="3306",
+        port=3306,
         user=username,
-        passwrd=password,
+        passwd=password,
         db=db_name,
     )
 
     cursor = connection.cursor()
     query = "SELECT cities.id,\
-                            cities.name,\
-                            states.name\
-                            FROM states INNER JOIN cities\
-                            ON state_id = states.id\
-                            ORDER BY cities.id;"
+            cities.name,\
+            states.name\
+            FROM states INNER JOIN cities\
+            ON state_id = states.id\
+            ORDER BY cities.id;"
     cursor.execute(query)
     rows = cursor.fetchall()
 
