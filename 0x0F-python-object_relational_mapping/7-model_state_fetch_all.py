8294dbeea7fddaad634df82c39fe5d4f7d0d4363
MDIMACat <romanmdima@gmail.com>
2023-11-17 11:41:02 +0200

    Created a session using sqlalchemy for class state

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Fetch all States from the given database.
+"""
+
+
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+
+
+if (__name__ == '__main__'):
+    engine = create_engine(f"mysql:///{argv[3]}", pool_per_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State)
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
