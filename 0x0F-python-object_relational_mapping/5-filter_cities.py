f904b9a3543a7eeaad1741bac2f34650199d05b6
MDIMACat <romanmdima@gmail.com>
2023-11-17 10:19:30 +0200

    Task 5 - revised with a with keyword

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,8 +7,7 @@ and lists all cities of that state, using the database
 import MySQLdb
 from sys import argv
 
-if (__name__ == "__main__"):
-
+if __name__ == "__main__":
     cities = ""
 
     username = argv[1]
@@ -16,27 +15,22 @@ if (__name__ == "__main__"):
     db_name = argv[3]
     state_name = argv[4]
 
-    connection = MySQLdb.connect(
+    with MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
         db=db_name,
-    )
+    ) as connection:
+        with connection.cursor() as cursor:
+            query = "SELECT cities.name \
+                     FROM states INNER JOIN cities \
+                     ON states.id = state_id \
+                     WHERE states.name = %s ORDER BY cities.id;"
 
-    cursor = connection.cursor()
-    query = "SELECT cities.name\
-                            FROM states INNER JOIN cities\
-                            ON states.id = state_id\
-                            WHERE states.name = %s ORDER BY cities.id;"
+            cursor.execute(query, (state_name,))
 
-    cursor.execute(query, (state_name,))
-    rows = cursor.fetchall()
-
-    for row in rows:
-        cities += f"{row[0]}, "
+            for row in cursor.fetchall():
+                cities += f"{row[0]}, "
 
     print(cities[0:-2])
-
-    cursor.close()
-    connection.close()
