5b83e7332d757b38fc0809b13963efc2102fc911
MDIMACat <romanmdima@gmail.com>
2023-11-17 12:08:36 +0200

    Task 7, changed format

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,14 +9,14 @@ from model_state import Base, State
 from sys import argv
 from sqlalchemy import create_engine
 
+if __name__ == '__main__':
+    engine = create_engine(f"mysql://{argv[1]}:{argv[2]}@localhost:\
+        3306/{argv[3]}", pool_per_ping=True)
 
-if (__name__ == '__main__'):
-    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost\
-        :3306/{argv[3]}", pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    Session = sessionmaker(engine)
-    with Session() as session:
-        states = session.query(State)
+    states = session.query(State).order_by(State.id).all()
 
-        for state in states:
-            print(f"{state.id}: {state.name}")
+    for state in states:
+        print(f"{state.id}: {state.name}")
