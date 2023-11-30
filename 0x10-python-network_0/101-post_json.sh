f9a452cb4e260bbfb477e43b5a7e3dd8bd38e1d8
MDIMACat <romanmdima@gmail.com>
2023-11-30 21:12:11 +0000

    Changed -x from lower case to upper case

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that Makes a POST request for a JSON file
-curl -s -x POST -H "Content-Type: application/json" -d @$2 $1
+curl -s -X POST -H "Content-Type: application/json" -d @$2 $1
