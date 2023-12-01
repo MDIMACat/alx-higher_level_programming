cd856884782c7765898d2e534920199623129f72
MDIMACat <romanmdima@gmail.com>
2023-12-01 18:41:35 +0000

    Using request module/package to get HTTP requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that fetches from a given URL"""
+
+import requests
+
+if __name__ == "__main__":
+
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print("Body response:")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t- content: {req.text}")
