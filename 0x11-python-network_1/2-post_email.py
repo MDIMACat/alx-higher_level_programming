4f2a99d503d6cdb4dc18208cab9a7fc8d4a15d7e
MDIMACat <romanmdima@gmail.com>
2023-12-01 17:43:27 +0000

    POST request using urllib module

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""POST script that post email to server"""
+
+from urllib.request import Request, urlopen
+from urllib import parse
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    email = argv[2]
+
+    data = parse.urlencode(email)
+    data = data.encode('ascii')
+
+    req = Request(url, data)
+    with urlopen(req) as response:
+        page = response.read().decode()
+        print(page)
