726fa5e06bfc71263372a6acf2dbb6a738c61187
MDIMACat <romanmdima@gmail.com>
2023-12-01 17:45:31 +0000

    POST request using urllib module, put the parse in one line

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
 
-    data = parse.urlencode(email)
-    data = data.encode('ascii')
+    data = parse.urlencode(email).encode('ascii')
 
     req = Request(url, data)
     with urlopen(req) as response:
