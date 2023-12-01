f40f2973e52cd416828d01dd16bfd5d3b6955b64
MDIMACat <romanmdima@gmail.com>
2023-12-01 18:24:31 +0000

    HTTP request that handles a HTTPError

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Handle Exceptions in HTTP Requests"""
+
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = argv[1]
+
+    req = Request(url)
+
+    try:
+        with urlopen(req) as response:
+            page = response.read().decode()
+            print(page)
+
+    except HTTPError as error:
+        print("Error code: ", error.code)
