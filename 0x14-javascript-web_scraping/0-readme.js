6a47d5e0c2fbf0b984c44c92e782b3c788386d4b
MDIMACat <romanmdima@gmail.com>
2024-02-20 12:43:46 +0200

    Javascript web scrapping - task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs')
+let filePath = process.argv[2]
+
+fs.readFile(filePath, 'utf-8', (err, inputData) => {
+    if (err) throw err;
+    console.log (inputData.toString());
+})
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,6 @@
+# 0x14-javascript-web_scraping
+
+ALX Higher level language stream, Javascript Web scrapping
+
+## Author
+Siduduzile Snenhlanhla Mdima
